7d66e02f61626bf83fc93b367d734463db94aa10
cryptoprofessor101 <lucifer.gn1@gmail.com>
2021-07-27 14:26:04 +0530

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: cryptoprofessor101 <lucifer.gn1@gmail.com>

--- run.sh
+++ run.sh
@@ -7,10 +7,10 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 #Path to k8s config file
-KUBECONFIG=/home/blockchain-automation-framework/build/config
+KUBECONFIG=/root/.kube/config
 
 #Test addition of Text
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
