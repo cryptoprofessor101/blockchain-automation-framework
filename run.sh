f5d9a3c395a808df60ec0772cc14c6de7dd11ef9
cryptoprofessor101 <lucifer.gn1@gmail.com>
2021-07-29 12:18:54 +0530

    [ci skip] Pushing deployment files for fabric restserver
    Signed-off-by: cryptoprofessor101 <lucifer.gn1@gmail.com>

--- run.sh
+++ run.sh
@@ -13,5 +13,5 @@ KUBECONFIG=/root/.kube/config
 
 
 echo "Running the playbook..."
-#exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
-exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework/examples/supplychain-app/configuration/deploy-supplychain-app.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
+#exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework/examples/supplychain-app/configuration/deploy-supplychain-app.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/supplychain.yaml" -e 'ansible_python_interpreter=/usr/bin/python3'
