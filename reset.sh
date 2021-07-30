5cbc22a898b04b9437c0d55bc78dca948be0f28a
cryptoprofessor101 <lucifer.gn1@gmail.com>
2021-07-30 04:37:22 +0000

    [ci skip] Deleting gitops release files
    Signed-off-by: cryptoprofessor101 <lucifer.gn1@gmail.com>

--- reset.sh
+++ reset.sh
@@ -11,6 +11,7 @@ KUBECONFIG=/root/.kube/config
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
+#exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
 #exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework//examples/supplychain-app/configuration/deploy-supplychain-app.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
+exec ansible-playbook -vv /home/blockchain-automation-framework/baf/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
 
