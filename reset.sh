af88570e74dd86aa4c14dc4eb21fe8029a842170
cryptoprofessor101 <lucifer.gn1@gmail.com>
2021-08-16 12:06:45 +0530

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: cryptoprofessor101 <lucifer.gn1@gmail.com>

--- reset.sh
+++ reset.sh
@@ -7,8 +7,11 @@ echo "Adding env variables..."
 export PATH=/root/bin:$PATH
 
 #Path to k8s config file
-KUBECONFIG=/home/blockchain-automation-framework/build/config
+KUBECONFIG=/root/.kube/config
 
 
 echo "Running the playbook..."
-exec ansible-playbook -vv /home/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
+exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
+#exec ansible-playbook -vv /home/ubuntu/baf/blockchain-automation-framework//examples/supplychain-app/configuration/deploy-supplychain-app.yaml --inventory-file=/home/ubuntu/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/ubuntu/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
+#exec ansible-playbook -vv /home/blockchain-automation-framework/baf/blockchain-automation-framework/platforms/shared/configuration/site.yaml --inventory-file=/home/blockchain-automation-framework/baf/blockchain-automation-framework/platforms/shared/inventory/ -e "@/home/blockchain-automation-framework/baf/blockchain-automation-framework/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -e "reset='true'"
+
